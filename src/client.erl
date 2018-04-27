bcd93f132e7e8b64d4158584733ed23ff737bb1d
chenxi1 <chenxi1@t4f-mbp-14221.local>
2018-04-27 19:14:10 +0800

    sdfsdfgfg

--- src/client.erl
+++ src/client.erl
@@ -1,50 +1,42 @@
--module(client).1
+-module(client).
 -export([client_login/1,send_message/1,logout/1]).
 -record(id,{currentid}).
 
 client_login({Id}) ->
     {ok,Socket} = gen_tcp:connect("localhost",2345,[binary,{packet,4}]),
     IdAtom=spawn_atom(Id),
-    register(IdAtom,spawn(fun() -> handle(Socket) end)),
-    %record current id
-    Cid = #id{currentid = Id},
-    _=Cid#id{},
+    Pid=spawn(fun() -> handle(Id,Socket) end),
+    register(IdAtom,Pid),
+    ok=gen_tcp:controlling_process(Socket,Pid),
+    IdAtom ! {self(),{login,Id}}.
 
-    IdAtom ! {self(),{login,Id}},
-        receive
-            {IdAtom, _Response} ->
-                ok
-        after 3000->
-            io:format("failed to login")
-        end.
 
 send_message({Id,Msg}) ->
-    Crid=#id{},
-    Cid=Crid#id.currentid,
-    IdAtom=spawn_atom(Cid),
-    IdAtom ! {self(),{msg,Id,Msg}},
-        receive
-            {IdAtom,_Response} ->
-                ok
-        after 3000->
-            io:format("failed to send")
-        end.
+    IdAtom=spawn_atom(Id),
+    IdAtom ! {self(),{chat,Id,Msg}}.
 
 logout(Id) ->
-    Crid=#id{},
-    if Id==Crid#id.currentid ->
         IdAtom=spawn_atom(Id),
-        IdAtom ! {self(),{logout,Id}},
-        receive
-            {client,Response} -> ok
-        end
-    end.
+        IdAtom ! {self(),{logout,Id}}.
+
 spawn_atom(Id)->
     Clientid="client"++integer_to_list(Id),
     list_to_atom(Clientid).
 
-handle(Socket) ->
+handle(CId,Socket) ->
     receive
+        {tcp,Socket,Bin} ->
+          io:format("received msg from server~p~n",Bin),
+          <<State:8,Body/binary>> = Bin,
+          case State of
+            0003->
+              <<Size:16,Body:Size/binary-unit:8>>=Body,
+
+          end,
+          %%gen_tcp:send(Socket,{chat_ok}),
+          handle(CId,Socket);
+        {tcp_closed,Socket}->
+          io:format("tcp connection closed~n");
         {From,Request} ->
             io:format("client proc received Request ~p~n",[Request]),
             case Request of
@@ -52,17 +44,17 @@ handle(Socket) ->
                 {login,Id} ->
                     I = term_to_binary(Id),
                     Packet = <<0000:8,(byte_size(I)):16,I/binary>>,
-                    gen_tcp:controlling_process(Socket,spawn(?MODULE,handle_tcpmsg,{From,Socket})),
                     ok=gen_tcp:send(Socket,Packet),
-                    handle(Socket);
+                    handle(CId,Socket);
                 %chat 0001
-                {msg,Id,Msg} ->
+                {chat,Id,Msg} ->
                     io:format("my message:~p~n",[Msg]),
+                    Sid=term_to_binary(CId),
                     Tid = term_to_binary(Id),
                     M = term_to_binary(Msg),
-                    Packet = <<0001:8,(byte_size(Tid)):16,Tid/binary,(byte_size(M)):16,M/binary>>,
+                    Packet = <<0001:8,(byte_size(Sid)):16,Sid/binary,(byte_size(Tid)):16,Tid/binary,(byte_size(M)):16,M/binary>>,
                     gen_tcp:send(Socket,Packet),
-                    handle(Socket);
+                    handle(CId,Socket);
                 %logout 0002
                 {logout,Id} ->
                     I = term_to_binary({Id}),
@@ -71,22 +63,9 @@ handle(Socket) ->
                     gen_tcp:close(Socket);
                 {_,_}->
                     io:format("wrong format cmd")
-            end
+            end;
+        M ->
+          io:format("~p here~n", [M])
     end.
 
-handle_tcpmsg(From,Socket)->
-    receive
-    %from server
-        {tcp,Socket,Bin} ->
-            <<_State:8,S/binary>> = Bin,
-            case binary_to_term(S) of
-                success ->
-                    From ! {client, "you have login successfully"},
-                    io:format("you have login successfully ~n");
-                failed ->
-                    From ! {"you have login failed,please try again"},
-                    gen_tcp:close(Socket)
-            end
-    after 5000 ->
-        ok
-    end.
+
--- src/serv.erl
+++ src/serv.erl
@@ -1,14 +1,12 @@
 -module(serv).
--export([start_server/0,initialize_ets/0,info_lookup/1,loop/1,info_update/3,init/1,terminate/2]).
+-export([start_server/0,initialize_ets/0,loop/1,init/1,terminate/2]).
 -include("user_info.hrl").
 -define(SERVER,?MODULE).
 
--record(state, {
+-record(users, {
     id
 }).
 
-start() ->
-    gen_server:start_link({local,?SERVER},?MODULE,[],[]).
 
 init([]) ->
     initialize_ets(),
@@ -25,58 +23,55 @@ start_server() ->
 handle_connect(Listen) ->
     {ok,Socket} = gen_tcp:accept(Listen),
     spawn(fun() -> handle_connect(Listen) end),
-    loop({Socket, #state{}}).
+    loop(Socket).
 
 initialize_ets() ->
-    ets:new(test,[set,public,named_table,{keypos,#user.name}]).
-    
+    ets:new(onlineusers,[set,public,named_table,{keypos,#users.id}]).
 
-info_lookup(Key) ->
-    ets:lookup(test,Key).
+lookup_ets(Id)->
+    ets:lookup(onlineusers,Id).
 
-info_update(Key,Pos,Update) ->
-    ets:update_element(test,Key,{Pos,Update}).
-
-loop({Socket,St}) ->
+loop(Socket) ->
     receive
         {tcp,Socket,Bin} ->
              <<State:8,Str/binary>> = Bin,            
             case State of
                 %login
-                    0000 ->                      
-                        <<Size:16,Body/binary>>=Str,  
-                            %S = binary_to_term(Size),
+                    0000 ->
+                      io:format("received login msg~n"),
+                        <<Size:16,Body:Size/binary-unit:8>>=Str,
                             Id=binary_to_term(Body),
                             Regid="user"++integer_to_list(Id),
                             IdAtom=list_to_atom(Regid),
-                            %register(IdAtom,self()),
-                            Suc = term_to_binary(sucess),                            
-                            Packet = <<0000:8,(byte_size(Suc)):16,Suc/binary>>, 
-                            ok=gen_tcp:send(Socket,Packet),
-                            loop({Socket, St#state{id=Id}});
+                            register(IdAtom,self()),
+                            NewUser=#users{id=Id},
+                            ets:insert(onlineusers,NewUser),
+                            loop(Socket);
                            
                 %chat
                     0001 ->
-                            <<Idsize:16,Id:Idsize/binary-unit:8,Msgsize:16,Msg:Msgsize/binary-unit:8>>=Str,
-                            N = term_to_binary({ok,received}),
-                            Len = byte_size(N),
-                            Packet = <<0001:8,Len:16,N/binary>>,
-                            gen_tcp:send(Socket,Packet),
-                            %转发给目标Pid
-                            Regid="user"+integer_to_list(binary_to_term(Id),
-                            IdAtom=list_to_atom(Regid),
-                            IdAtom!{Msg},
-                            loop({Socket,State});
+                      io:format("received chat msg~n"),
+                            <<Sidsize:16,Sid:Sidsize/binary-unit:8,Tidsize:16,Tid:Tidsize/binary-unit:8,
+                              Msgsize:16,Msg:Msgsize/binary-unit:8>>=Str,
+                            %send to tgt Pid
+                            Regid="user"+integer_to_list(binary_to_term(Tid)),
+                            case lookup_ets(binary_to_term(Tid)) of
+                              [_Record]->
+                                IdAtom=list_to_atom(Regid),
+                                IdAtom!{binary_to_term(Sid),Msg};
+                              []->
+                                io:format("target user not online~n")
+                            end,
+                            loop(Socket);
                 %logout
                     0002 ->
-                        %<<Idsize:16,Id:IdSize/binary-unit:8>>=Str,   
-                        N = term_to_binary(ok),
-                        Packet = <<0002:8,(byte_size(N)):16,N/binary>>,
-                        gen_tcp:send(Socket,Packet)
+                      io:format("received logout msg~n")
+                        %<<Idsize:16,Id:IdSize/binary-unit:8>>=Str,
             end;
 
         {tcp_closed,Socket} ->
                               io:format("Server socket closed~n");
-        Msg->
-            io:format("~p~n",Msg)
+        {Srcid,Msg}->
+            io:format("receive msg from ~p :~p~n",Srcid,Msg),
+            gen_tcp:send()
     end.
